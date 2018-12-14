219fdd9a0bb18e69b82a19ee072f997ddeca5ce9
Rami Mhanna <rami.mhanna@powercoders.org>
2018-12-14 16:39:30 +0100

    JS v1.3

--- scratch/js_problem/setClass.js
+++ scratch/js_problem/setClass.js
@@ -1,20 +1,32 @@
-console.log("my code is here!!!");
-
-function greet(name, time) {
-  console.log('Good ' + time +" "+ name);
+// the className :"is the class name that we want to assign it to the element."
+function setClass(event,idName,className) {
+  let el = document.getElementById(idName);
+  el.className = className;
 }
-
-function setClassWarning(event) {
+// the className :"is the class name that we want to assign it to the element."
+function setClass(event,className) {
   let el = document.getElementById('mypara');
-  el.className = 'warning';
+  el.className = className;
 }
+//console.log("my code is here!!!");
 
-function clearClass(event) {
-  let el = document.getElementById('mypara');
-  el.className = '';
-}
-
-function setClassTip(event){
-  let el = document.getElementById('mypara');
-  el.className = 'tip';
-}
+//function greet(name, time) {
+//  console.log('Good ' + time +" "+ name);
+//}
+// "change the  class name of the element with 'mypara' id to the 'warning' class"
+//function setClassWarning(event) {
+//let el = document.getElementById('mypara');
+//el.className = 'warning';
+//}
+// "change the  class name of the element with 'mypara' id"
+//function clearClass(event) {
+//  let el = document.getElementById('mypara');
+//  el.className = '';
+//}
+// "change the  class name of the element with 'mypara' id to the 'tip' class"
+//function setClassTip(event){
+//  let el = document.getElementById('mypara');
+//  el.className = 'tip';
+//  console.log(event);
+//}
+// the idName :"is connected to the element that we want to change its class."
