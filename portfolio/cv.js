1ac680f5299e07409f99941c0bbfde39f10f8b7f
Rami Mhanna <rami.mhanna@powercoders.org>
2018-12-15 00:35:16 +0100

    CV with 3 styles for 3 sizes with js

---
+++ portfolio/cv.js
@@ -0,0 +1,69 @@
+function show_personal_information() {
+  document.getElementById('personal_information').style.display = "block";
+  document.getElementById('it_knowledge').style.display = "none";
+  document.getElementById('education').style.display = "none";
+  document.getElementById('courses').style.display = "none";
+  document.getElementById('activities').style.display = "none";
+  document.getElementById('qualification').style.display = "none";
+  document.getElementById('languages').style.display = "none";
+}
+
+function show_it_knowledge() {
+  document.getElementById('it_knowledge').style.display = "block";
+  document.getElementById('personal_information').style.display = "none";
+  document.getElementById('education').style.display = "none";
+  document.getElementById('courses').style.display = "none";
+  document.getElementById('activities').style.display = "none";
+  document.getElementById('qualification').style.display = "none";
+  document.getElementById('languages').style.display = "none";
+}
+
+function show_education() {
+  document.getElementById('education').style.display = "block";
+  document.getElementById('it_knowledge').style.display = "none";
+  document.getElementById('personal_information').style.display = "none";
+  document.getElementById('courses').style.display = "none";
+  document.getElementById('activities').style.display = "none";
+  document.getElementById('qualification').style.display = "none";
+  document.getElementById('languages').style.display = "none";
+}
+
+function show_courses() {
+  document.getElementById('courses').style.display = "block";
+  document.getElementById('it_knowledge').style.display = "none";
+  document.getElementById('education').style.display = "none";
+  document.getElementById('personal_information').style.display = "none";
+  document.getElementById('activities').style.display = "none";
+  document.getElementById('qualification').style.display = "none";
+  document.getElementById('languages').style.display = "none";
+}
+
+function show_activities() {
+  document.getElementById('activities').style.display = "block";
+  document.getElementById('it_knowledge').style.display = "none";
+  document.getElementById('education').style.display = "none";
+  document.getElementById('courses').style.display = "none";
+  document.getElementById('personal_information').style.display = "none";
+  document.getElementById('qualification').style.display = "none";
+  document.getElementById('languages').style.display = "none";
+}
+
+function show_qualification() {
+  document.getElementById('qualification').style.display = "block";
+  document.getElementById('it_knowledge').style.display = "none";
+  document.getElementById('education').style.display = "none";
+  document.getElementById('courses').style.display = "none";
+  document.getElementById('activities').style.display = "none";
+  document.getElementById('personal_information').style.display = "none";
+  document.getElementById('languages').style.display = "none";
+}
+
+function show_languages() {
+  document.getElementById('languages').style.display = "block";
+  document.getElementById('it_knowledge').style.display = "none";
+  document.getElementById('education').style.display = "none";
+  document.getElementById('courses').style.display = "none";
+  document.getElementById('activities').style.display = "none";
+  document.getElementById('qualification').style.display = "none";
+  document.getElementById('personal_information').style.display = "none";
+}
