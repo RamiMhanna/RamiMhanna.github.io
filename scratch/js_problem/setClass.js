262e9670dcd6520cbb0eb5b3e905af113ab57a6f
Rami Mhanna <rami.mhanna@powercoders.org>
2018-12-25 00:32:13 +0100

    Appling addEventListener() method

--- scratch/js_problem/setClass.js
+++ scratch/js_problem/setClass.js
@@ -1,32 +1,30 @@
 // the className :"is the class name that we want to assign it to the element."
-function setClass(event,idName,className) {
-  let el = document.getElementById(idName);
-  el.className = className;
+function setClass(className) {
+  //let el = document.getElementById('mypara');
+  //el.className = className;
+  //Method and Property Chaining
+  document.getElementById('mypara').className = className;
 }
-// the className :"is the class name that we want to assign it to the element."
-function setClass(event,className) {
-  let el = document.getElementById('mypara');
-  el.className = className;
-}
-//console.log("my code is here!!!");
 
-//function greet(name, time) {
-//  console.log('Good ' + time +" "+ name);
-//}
-// "change the  class name of the element with 'mypara' id to the 'warning' class"
-//function setClassWarning(event) {
-//let el = document.getElementById('mypara');
-//el.className = 'warning';
-//}
-// "change the  class name of the element with 'mypara' id"
-//function clearClass(event) {
-//  let el = document.getElementById('mypara');
-//  el.className = '';
-//}
-// "change the  class name of the element with 'mypara' id to the 'tip' class"
-//function setClassTip(event){
-//  let el = document.getElementById('mypara');
-//  el.className = 'tip';
-//  console.log(event);
-//}
-// the idName :"is connected to the element that we want to change its class."
+document.addEventListener('DOMContentLoaded', function (event){
+  //let makeWarning = document.getElementById('make-warning');
+  //makeWarning.addEventListener('click', function (event){
+  //Method and Property Chaining
+  document.getElementById('make-warning').addEventListener('click', function (event){
+    setClass('warning');
+  });
+
+  //Method and Property Chaining
+  //let makeTip = document.getElementById('make-tip');
+  //makeTip.addEventListener('click', function (event){
+  document.getElementById('make-tip').addEventListener('click', function (event){
+    setClass('tip');
+  });
+
+  //Method and Property Chaining
+  //let makeNormal = document.getElementById('make-normal');
+  //makeNormal.addEventListener('click', function (event){
+  document.getElementById('make-normal').addEventListener('click', function (event){
+    setClass('');
+  });
+});
