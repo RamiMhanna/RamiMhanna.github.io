f8198248e0ad902b6d8a56eaa049ff709cda50fb
Rami Mhanna <rami.mhanna@powercoders.org>
2019-01-10 14:43:12 +0100

    -- Create shopping list --
    Adding the new list Item by clicking on the ' Click me ' button, or by pressing the Enter key.
    If the input box is empty then, pressing the Enter key or clicking the add button will give the user an alert message to inform him that the there is nothing inside the input box.

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,7 +1,33 @@
+// Make sure that the DOM contents is Loaded.
+document.addEventListener('DOMContentLoaded', function () {
+  let item = document.getElementById('item');
+  let ul = document.querySelector('ul');
+  let btn = document.querySelector('button');
+  item.addEventListener('keyup', function (event) {
+    if ((event.key === 'Enter') && (createNewListItem(item.value) !== null)) {
+      ul.appendChild(createNewListItem(item.value));
+      item.value = '';
+    }
+  });
+  // check if the add button is clicked.
+  btn.addEventListener('click', function () {
+    // if the Value in the Item box is not empty then add new item.
+    if (item.value !== '') {
+      ul.appendChild(createNewListItem(item.value));
+      item.value = '';
+    }
+    // if the Value in the item box is empty, then show alert message.
+    else {
+      alert('You have to enter some text');
+    }
+  });
+});
+
 // Create a new list element.
 function createNewListItem(newItem) {
   // Check if the newItem is  empty.
   if ((newItem === '')) {
+    alert('You have to enter some text');
     return null;
   }
   // If the newItem is not empty.
@@ -12,28 +38,27 @@ function createNewListItem(newItem) {
     span.innerText = newItem;
     li.appendChild(span);
     // Adding an input element to the li and hide it ,in case we wanted to change the value.
-    let input = document.createElement('input');
-    li.appendChild(input);
-    input.className = 'hide ';
-
+    let inputBox = document.createElement('input');
+    li.appendChild(inputBox);
+    inputBox.className = 'hide ';
     // Check if the list item is clicked and switch it to input box to edit its value.
     span.addEventListener('click', function () {
-      input.className = '';
+      inputBox.className = '';
       span.className = 'hide';
-      input.focus();
-      input.value = span.innerText;
-      input.setSelectionRange(0, input.value.length);
+      inputBox.focus();
+      inputBox.value = span.innerText;
+      inputBox.setSelectionRange(0, inputBox.value.length);
     });
     // After editing the Value , if the key Enter is pressed switch the input box to the span element(list item).
-    input.addEventListener('keypress', change);
+    inputBox.addEventListener('keypress', change);
 
     function change(event) {
       console.log(event.which);
-      if (event.which === 13) {
+      if (event.key === 'Enter') {
         // If the new value is not empty, then switch.
-        if (input.value !== '') {
-          input.previousElementSibling.innerText = input.value;
-          input.className = 'hide';
+        if (inputBox.value !== '') {
+          inputBox.previousElementSibling.innerText = inputBox.value;
+          inputBox.className = 'hide';
           span.className = '';
           // If the new Value is empty, then delete the list item.
         } else {
@@ -45,20 +70,3 @@ function createNewListItem(newItem) {
     return li;
   }
 }
-
-// Make sure that the DOM contents is Loaded.
-document.addEventListener('DOMContentLoaded', function () {
-  // check if the add button is clicked.
-  document.querySelector('button').addEventListener('click', function () {
-    // if the Value in the Item box is not empty then add new item.
-    if (document.getElementById('item').value !== '') {
-      document.querySelector('ul').appendChild(createNewListItem(document.getElementById('item').value));
-      document.getElementById('item').value = '';
-    }
-    // if the Value in the item box is empty, then show alert message.
-    else {
-      alert('You have to enter some text');
-    }
-  });
-});
-
