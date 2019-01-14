cd34820d3bce3e01488c166fb82ceb76385bc237
Rami Mhanna <rami.mhanna@powercoders.org>
2019-01-14 20:19:53 +0100

    -- Create shopping list --
    Changing the design of the Shopping List.
    
    Adding the new list Item by clicking on the ' Click me ' button, or by pressing the Enter key.
    If the input box is empty, then pressing the Enter key will do nothing and the add button will become disabled .

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,72 +1,59 @@
-// Make sure that the DOM contents is Loaded.
 document.addEventListener('DOMContentLoaded', function () {
-  let item = document.getElementById('item');
-  let ul = document.querySelector('ul');
-  let btn = document.querySelector('button');
+  const item = document.getElementById('item');
+  const ul = document.querySelector('ul');
+  const btn = document.querySelector('button');
+
   item.addEventListener('keyup', function (event) {
-    if ((event.key === 'Enter') && (createNewListItem(item.value) !== null)) {
-      ul.appendChild(createNewListItem(item.value));
-      item.value = '';
+    const trimmedvalue = item.value.trim();
+    btn.disabled = item.value.trim() === '';
+
+    if (trimmedvalue === '') {
+      return;
     }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    ul.appendChild(createNewListItem(trimmedvalue));
+    item.value = '';
+    btn.disabled = true;
   });
-  // check if the add button is clicked.
+
   btn.addEventListener('click', function () {
-    // if the Value in the Item box is not empty then add new item.
-    if (item.value !== '') {
-      ul.appendChild(createNewListItem(item.value));
-      item.value = '';
-    }
-    // if the Value in the item box is empty, then show alert message.
-    else {
-      alert('You have to enter some text');
-    }
+    const trimmedvalue = item.value.trim();
+
+    ul.appendChild(createNewListItem(trimmedvalue));
+    item.value = '';
+    btn.disabled = true;
+    item.focus();
   });
+  btn.disabled = true;
+  item.focus();
 });
 
-// Create a new list element.
+/**
+ * create and return an 'li' element for inclusion in shopping list.
+ *
+ * the li element that is returned has the structure :
+ * '&lt;li><span>itemName</span> <button>Delete</button></li>'.
+ *
+ * @param {string} newItem name of the item to add to the list.
+ * @returns {HTMLElement} li element.
+ */
+
 function createNewListItem(newItem) {
-  // Check if the newItem is  empty.
-  if ((newItem === '')) {
-    alert('You have to enter some text');
-    return null;
-  }
-  // If the newItem is not empty.
-  else {
-    // Add a new item.
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    span.innerText = newItem;
-    li.appendChild(span);
-    // Adding an input element to the li and hide it ,in case we wanted to change the value.
-    let inputBox = document.createElement('input');
-    li.appendChild(inputBox);
-    inputBox.className = 'hide ';
-    // Check if the list item is clicked and switch it to input box to edit its value.
-    span.addEventListener('click', function () {
-      inputBox.className = '';
-      span.className = 'hide';
-      inputBox.focus();
-      inputBox.value = span.innerText;
-      inputBox.setSelectionRange(0, inputBox.value.length);
-    });
-    // After editing the Value , if the key Enter is pressed switch the input box to the span element(list item).
-    inputBox.addEventListener('keypress', change);
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const deleteBox = document.createElement('button');
 
-    function change(event) {
-      console.log(event.which);
-      if (event.key === 'Enter') {
-        // If the new value is not empty, then switch.
-        if (inputBox.value !== '') {
-          inputBox.previousElementSibling.innerText = inputBox.value;
-          inputBox.className = 'hide';
-          span.className = '';
-          // If the new Value is empty, then delete the list item.
-        } else {
-          this.parentElement.remove();
-        }
-      }
-    }
+  deleteBox.addEventListener('click', function () {
+    li.remove();
+  });
 
-    return li;
-  }
+  span.innerText = newItem;
+  li.appendChild(span);
+  deleteBox.innerText = 'Delete';
+  li.appendChild(deleteBox);
+  return li;
 }
