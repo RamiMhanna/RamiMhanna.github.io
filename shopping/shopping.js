2cecf598c672f35e6ba29fee4b7a352d9174fbe9
Rami Mhanna <rami.mhanna@powercoders.org>
2019-01-28 11:01:34 +0100

    -- Add features to the shopping list --
    Add delete Button to delete all the list elements that we have created.
    and this button will disappear when we have one element in the list or when our list elements is empty.

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -2,8 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const item = document.getElementById('item');
   const ul = document.querySelector('ul');
   const btn = document.querySelector('button');
+  const deleteAll = document.getElementById('delAll');
+  deleteAll.className = 'hidden';
 
-  item.addEventListener('keyup', function (event) {
+  deleteAll.addEventListener('click', function () {
+    document.querySelectorAll('li').forEach(function (li) {
+      li.remove();
+    });
+    hideDelBtn();
+    item.focus();
+  });
+
+  item.addEventListener('keyup', function () {
     const trimmedvalue = item.value.trim();
     btn.disabled = item.value.trim() === '';
 
@@ -18,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ul.appendChild(createNewListItem(trimmedvalue));
     item.value = '';
     btn.disabled = true;
+    hideDelBtn();
   });
 
   btn.addEventListener('click', function () {
@@ -27,10 +38,12 @@ document.addEventListener('DOMContentLoaded', function () {
     item.value = '';
     btn.disabled = true;
     item.focus();
+    hideDelBtn();
   });
   btn.disabled = true;
   item.focus();
 });
+
 
 /**
  * create and return an 'li' element for inclusion in shopping list.
@@ -41,7 +54,6 @@ document.addEventListener('DOMContentLoaded', function () {
  * @param {string} newItem name of the item to add to the list.
  * @returns {HTMLElement} li element.
  */
-
 function createNewListItem(newItem) {
   const li = document.createElement('li');
   const span = document.createElement('span');
@@ -49,11 +61,25 @@ function createNewListItem(newItem) {
 
   deleteBox.addEventListener('click', function () {
     li.remove();
+    hideDelBtn();
   });
-
   span.innerText = newItem;
   li.appendChild(span);
   deleteBox.innerText = 'Delete';
   li.appendChild(deleteBox);
   return li;
 }
+
+function hideDelBtn() {
+  if (document.querySelectorAll('li').length < 2) {
+    document.getElementById('delAll').className = 'hidden';
+  } else {
+    document.getElementById('delAll').className = '';
+  }
+}
+
+/*
+       .__(.)< (MEOW)
+        \___)
+ ~~~~~~~~~~~~~~~~~~
+ */
