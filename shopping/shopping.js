9ad1715513594db7e3508ce97459c04ded0b90db
Rami Mhanna <rami.mhanna@powercoders.org>
2019-02-04 11:46:33 +0100

    -- Add Class to the shopping list --
    Add an class called ShoppingListItem to the js code and add the constructor to construct an object contain the name of the item and the quantity of it .
     defining a method called toListItem() instead of the function createNewListItem() to applying on the object.

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -48,7 +48,6 @@ function domContentLoaded() {
 
     const product = new ShoppingListItem(trimmedvalue, quantity.value.trim());
 
-
     ul.appendChild(product.toListItem());
     quantity.value = '';
     item.value = '';
@@ -71,15 +70,26 @@ if (document.readyState === 'loading') {
 
 /**
  * Represents an item in the shopping list.
- *
- * @param  {string} name  name of the item.
- * @param  {string} quantity  of the item.
- * @constructor
  */
-function ShoppingListItem(name, quantity) {
-  this.name = name;
-  this.quantity = quantity;
-  this.toListItem = function () {
+class ShoppingListItem {
+  /**
+   * @param  {string} name  name of the item.
+   * @param  {string} quantity  of the item.
+   */
+  constructor(name, quantity) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+
+  /**
+   * THis method is work on the objects which has the type ShoppingListItem.
+   * * the li element that is returned has the structure :
+   * '&lt;li><span>itemName</span><span>Quantity</span> <button>Delete</button></li>'.
+   * In this method also we are listening to the click event on the delete button, to delete the element.
+   *
+   * @returns {HTMLElement} li element.
+   */
+  toListItem() {
     const li = document.createElement('li');
     const span = document.createElement('span');
     const deleteBox = document.createElement('button');
@@ -102,43 +112,9 @@ function ShoppingListItem(name, quantity) {
     deleteBox.innerText = 'Delete';
     li.appendChild(deleteBox);
     return li;
-  };
+  }
+
 }
-
-
-/**
- * create and return an 'li' element for inclusion in shopping list.
- *
- * the li element that is returned has the structure :
- * '&lt;li><span>itemName</span> <button>Delete</button></li>'.
- * @param {ShoppingListItem} product , The Item to append yo the list.
- * @returns {HTMLElement} li element
- */
-
-// function createNewListItem(product) {
-//   const li = document.createElement('li');
-//   const span = document.createElement('span');
-//   const deleteBox = document.createElement('button');
-//
-//   deleteBox.addEventListener('click', function () {
-//     li.remove();
-//     hideDelBtn();
-//   });
-//
-//   span.innerText = product.name;
-//   li.appendChild(span);
-//
-//   if (product.quantity !== "") {
-//     const amount = document.createElement('span');
-//     const qty = document.createTextNode(`( ${product.quantity} )`);
-//     amount.appendChild(qty);
-//     li.appendChild(amount);
-//   }
-//
-//   deleteBox.innerText = 'Delete';
-//   li.appendChild(deleteBox);
-//   return li;
-// }
 
 /**
  * Hide the button that Delete all the list elements in the Shopping List.
